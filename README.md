6a77f17f411a91dcdc6c3acb12a1e1afb06213ff
Alexis Isaya <87978187+AlexIsaya@users.noreply.github.com>
2025-09-23 22:45:04 -0300

    update files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Alura-Challenge-AmigoSecreto
-Proyecto que permite practicar los fundamentos de programación, se encuentra desarrollado con HTML, CSS y Javascript. La idea principal es registrar nombres de amigos para luego realizar un sorteo y poder visualizar de manera aleatoria el amigo secreto sorteado. Es importante destacar que no se pueden dejar el campo vacío, ingresar números o ingresar nombres repetidos.
+Proyecto que permite practicar los fundamentos de programación, se encuentra desarrollado con HTML, CSS y Javascript. Elaborado en conjunto con Alura Latam para el programa ONE de Oracle. 
+La idea principal es registrar nombres de amigos para luego realizar un sorteo y poder visualizar de manera aleatoria el amigo secreto sorteado. Es importante destacar que no se pueden dejar el campo vacío, ingresar números o ingresar nombres repetidos.
--- app.js
+++ app.js
@@ -1,1 +1,62 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if( nombreAmigo.trim() === '' ){
+        alert('Por favor ingresa el nombre de tu amigo (No esta permitido dejar el campo en blanco)');
+        
+    }
+    else{
+        if((parseInt(nombreAmigo))){
+            alert('Por favor ingresa el nombre de tu amigo (No esta permitido ingresar numeros)');            
+            
+        }else
+            {
+            // asignar valor al array
+            validarAmigo(nombreAmigo);            
+            }
+            limpiarCampo();
+        }
+        imprimirLista();
+    
+    return;
+}
+
+function validarAmigo(elemento){
+    if (amigos.includes(elemento)){
+        alert(`Tu amigo ${elemento} ya se encuentra registrado, por favor intenta con otro nombre.`);        
+    }
+    else{
+          amigos.push(elemento);         
+        }
+    return;
+}
+
+function imprimirLista(){
+    let listado = document.getElementById('listaAmigos');
+    listado.innerHTML = '';
+    for(let item of amigos){
+        listado.innerHTML += '<li>'+ item + '<li>';
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if(amigos.length <= 1 ){
+        alert('No hay amigos registrados para sortear, por favor registre sus amigos.');
+    }
+    else{
+        let indice = Math.floor(Math.random() * amigos.length);
+        let nombreAleatorio = amigos[indice];
+        let result = document.getElementById('resultado');
+        result.innerHTML = '<li> Tu amigo secreto es:'  + nombreAleatorio + '</li>'
+        return;
+    }
+}
+
+function limpiarCampo(){
+    document.getElementById('amigo').value = '';
+    return;
+}
